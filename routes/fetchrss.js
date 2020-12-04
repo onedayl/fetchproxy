62e3e86df04d09dfc93a9b4c19195dd3f20206d3
onedayl <onedayleung@gmail.com>
2020-12-04 17:55:31 +0800

    fix img content

--- routes/fetchrss.js
+++ routes/fetchrss.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const axios = require('axios')
 const rssParser = require('rss-parser')
-const { response } = require('express')
 
 const router = express.Router()
 const parser = new rssParser()
@@ -12,13 +11,17 @@ router.get('/:rssId?', async (req, res, next) => {
       const response = await axios.get(`http://fetchrss.com/rss/${req.params.rssId}.xml`)
       const feed = await parser.parseString(response.data)
       res.locals.status = 200
+      console.log(feed.items[0])
       res.locals.data = feed.items.map(item => {
+        const contentLength = item.content.length
         const snippetLength = item.contentSnippet.length
         return {
           title: item.title,
           link: item.link,
           pubDate: item.pubDate,
-          content: snippetLength == 30 ? '' : item.contentSnippet.slice(0, snippetLength - 31)
+          content: snippetLength == 30
+            ? item.content.slice(10, contentLength - 144)
+            : item.contentSnippet.slice(0, snippetLength - 31)
         }
       })
       next()
